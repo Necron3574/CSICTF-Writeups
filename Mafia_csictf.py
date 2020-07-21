b8b4d7375168a62052104c588603230ba6fea433
Kaustubh BM <kaustubhbm3574@gmail.com>
2020-07-21 16:44:32 +0530

    Update Mafia_csictf.py

--- Mafia_csictf.py
+++ Mafia_csictf.py
@@ -1,7 +1,13 @@
+#This was a very interesting challenge.
+#My idea behind this was to lower the range of potential people from 300 to something lesser and then binary search the amount of cash since we have only 1000 tries.
+#So first I looped through all 300 people and checked for those who had money > 900000. I added all those people to a list.
+#After this I was left with 35 potential people. So I binary searched all of them and submitted the highest result among all of them to get the flag.
+#Here's my code.(It can be more efficient but meh)
+
 import math
 from pwn import *
 
-def binary_searcher(i,up,down):
+def cash_finder(i,up,down):
     while True:
         mid = (up+down)//2
         r.sendline('1 '+ str(i) + ' ' + str(mid))
